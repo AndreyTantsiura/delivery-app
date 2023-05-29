3740b0ee4c587ac016c1e19a6a1370503ab69fb1
Andrii Tantsiura <andrey.tantsyura1980@gmail.com>
2023-05-29 14:20:31 +0300

    fix: deploy page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import {
   SelectedCardId,
   SelectedShopProvider,
@@ -16,9 +16,9 @@ root.render(
     <SelectedShopProvider>
       <SelectedCardId>
         <TotalCountProvider>
-          <Router>
+          <HashRouter>
             <App />
-          </Router>
+          </HashRouter>
         </TotalCountProvider>
       </SelectedCardId>
     </SelectedShopProvider>
